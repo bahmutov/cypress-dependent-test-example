f6e80ba697c69384aa03850795e8b549fadd4c6d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-19 14:54:15 -0400

    using cypress-plugin-config

---
+++ cypress/e2e/spec2.cy.js
@@ -0,0 +1,30 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+/// <reference types="cypress-data-session" />
+
+import {
+  getPluginConfigValue,
+  setPluginConfigValue,
+} from 'cypress-plugin-config'
+
+it('creates item A', () => {
+  cy.log('a very long test')
+  cy.wait(1000)
+  cy.log('that creates project').then(() => {
+    // how do we pass the data created in this test
+    // to the next test?
+    const projectName = `my random project ${Cypress._.random(1e4)}`
+    cy.log(projectName)
+    setPluginConfigValue('project name', projectName)
+  })
+})
+
+it('continues working with data created in the previous test', () => {
+  const projectName = getPluginConfigValue('project name')
+  expect(projectName, 'got project name').to.be.a(
+    'string',
+    'project name created by previous test',
+  )
+  cy.wrap(projectName, { timeout: 0 }).should('include', 'project')
+})
